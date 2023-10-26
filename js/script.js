014c7dc807b61d2ec852a512151cfb7b9d662eb4
M. Auliya Mirzaq Romdloni <mirzaqarjap@gmail.com>
2023-10-26 21:39:45 +0700

    feat: add burger menu

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@ AUTHOR_TOGGLE.addEventListener("click", () => {
 });
 
 const NAV_LINKS = document.querySelectorAll(".nav-link");
+const BURGER = document.querySelector("#burger-button .burger");
 
 window.addEventListener("scroll", () => {
   if (
@@ -14,9 +15,21 @@ window.addEventListener("scroll", () => {
     NAV_LINKS.forEach((link) => {
       link.classList.add("contrast");
     });
+    BURGER.classList.add("contrast");
   } else {
     NAV_LINKS.forEach((link) => {
       link.classList.remove("contrast");
     });
+    BURGER.classList.remove("contrast");
   }
+});
+
+const BURGER_BUTTON = document.querySelector("#burger-button");
+const NAV = document.querySelector("header .header-container nav");
+const HEADER = document.querySelector("header .header-container");
+
+BURGER_BUTTON.addEventListener("click", () => {
+  BURGER_BUTTON.classList.toggle("active");
+  NAV.classList.toggle("active");
+  HEADER.classList.toggle("active");
 });
